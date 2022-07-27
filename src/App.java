beb23cbee6a045dc7aa1c57c3cf5eebbec869ea2
cursoskanechan <kanes.chan@gmail.com>
2022-07-27 09:48:48 -0300

    Interação tradicional for/for-each

--- src/App.java
+++ src/App.java
@@ -1,5 +1,27 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        List<Integer> lista = Arrays.asList(1, 6, 4, 3, 2, 6, 8, 0, 4, 3, 1);
+
+        System.out.println("\n--------Laço For--------\n");
+        
+        //Loop tradicional
+        for(Iterator<Integer> numero = lista.iterator(); numero.hasNext();){
+            Integer integer = numero.next();
+            System.out.println(integer);
+        }
+
+        System.out.println("\n--------For-Each--------\n");
+
+        //For-Each
+        for (Integer numero : lista) {
+            System.out.println(numero);
+        }
+
     }
 }
